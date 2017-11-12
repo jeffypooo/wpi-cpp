a00de0b6440b8c5482a654766e9c9921e87250ba
Jeff Jones <jeff@beartooth.com>
2017-11-11 22:04:26 -0700

    add some inline comments

--- src/example/main.cpp
+++ src/example/main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <thread>
-#include <unistd.h>
 #include <cstring>
 #include "../io/spi.h"
 #include "../io/logger.h"
 
 #define ERR_SPI_INIT_FAILURE    (-1)
 #define ERR_SPI_XFER_FAILURE    (-2)
+#define DEFAULT_MSG             "The quick brown fox jumps over the lazy dog."
+#define DEFAULT_SPI_SPD         100000
 
 using namespace io;
 
-static const int SPI_SPD_HZ = 100000;
 static const char *TAG = "MAIN";
 
 void SPIDemoLoop(const std::string &msg, io::SPI &spi);
 
 int main(int argc, char **argv) {
-    std::string message("The quick brown fox jumps over the lazy dog.");
-    int speed = SPI_SPD_HZ;
+    // default values
+    std::string message(DEFAULT_MSG);
+    int speed = DEFAULT_SPI_SPD;
+    // some rudimentary args processing
+    // -m "message here" to change the message sent via SPI
+    // -s <speed_hz> to change the SPI speed
     if (argc > 1) {
         for (int i = 1; i < argc; ++i) {
             if (strcmp("-m", argv[i]) == 0 && (i + 1) < argc) {
@@ -29,6 +33,7 @@ int main(int argc, char **argv) {
             }
         }
     }
+    // init the driver and check that it configured without error
     SPI spi(SPIChannel::ZERO, speed);
     if (!spi.IsReady()) {
         LogE(TAG, "SPI setup failed, exiting...");
@@ -46,11 +51,13 @@ int main(int argc, char **argv) {
 void SPIDemoLoop(const std::string &msg, io::SPI &spi) {
     auto str_size = msg.size();
     while (true) {
+        // wait for the user to press enter to send the data
         LogV(TAG, "press enter to send data...");
         char temp = 'x';
         while (temp != '\n') {
             std::cin.get(temp);
         }
+        // send data, exiting if a failure occurs
         auto *data_buf = new uint8_t[str_size];
         memcpy(data_buf, msg.data(), str_size);
         int xfer_cnt = spi.Transfer(data_buf, str_size);
